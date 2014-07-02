71715b276f8eeca0ff7d381f85dfe49dace85590
ubaldino zurita <ozzirisc@gmail.com>
2014-07-02 12:43:02 +0000

    codigos implementados blink

--- prueba2.py
+++ prueba2.py
@@ -1,1 +1,18 @@
-print "Hola"
+import serial , sys , time
+
+ser = serial.Serial( port="/dev/ttyUSB0" , baudrate=9600 )
+
+#print sys.argv[1]
+"""
+if sys.argv[1] == "on":
+  ser.write( 'a' )
+
+if sys.argv[1] == "off":
+  ser.write( 'b' )
+"""
+
+while( True ):
+  ser.write( 'a' )
+  time.sleep( .5 )
+  ser.write( 'b' )
+  time.sleep( .5 )
